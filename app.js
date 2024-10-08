22a67042a9264204c60eeb564921f37e96df6e51
niharika896 <niharika9agarwal@gmail.com>
2024-10-08 20:32:37 +0530

    submit

--- app.js
+++ app.js
@@ -3,47 +3,40 @@ const np = document.getElementById("np");
 let ka = 0;
 let na = 0;
 
-window.onload = function() {
-    if (localStorage.getItem('ka')) {
-        ka = Number(localStorage.getItem('ka'));
-        document.getElementById('khushi').innerText = ka;
-    }
-    if (localStorage.getItem('na')) {
-        na = Number(localStorage.getItem('na'));
-        document.getElementById('niharika').innerText = na; 
-    }
-};
 
 document.getElementById("submit").addEventListener('click', () => {
-    try{
         let amount = Number(document.getElementById("money").value);
-        document.getElementById("popup").style.display = "none";
-    
-        if (document.getElementById("popup").dataset.source === "kp") {
-            ka += amount;
+        if(isNaN(amount)){
+            alert("please enter valid input")
+        }
+        else{
+
+            document.getElementById("popup").style.display = "none";
+            
+            if (document.getElementById("popup").dataset.source === "kp") {
+                ka += amount;
+                localStorage.setItem('ka', ka);
+                document.getElementById("khushi").innerText = `${ka}`;
+            } else if (document.getElementById("popup").dataset.source === "np") {
+                na += amount;
+                localStorage.setItem('na', na);
+                document.getElementById("niharika").innerText = `${na}`;
+            }
+            
+            if (ka >= na) {
+                ka -= na;
+                na = 0;
+            } else {
+                na -= ka;
+                ka = 0;
+            }
+            document.getElementById("khushi").innerText = ka;
+            document.getElementById("niharika").innerText = na;
             localStorage.setItem('ka', ka);
-            document.getElementById("khushi").innerText = `${ka}`;
-        } else if (document.getElementById("popup").dataset.source === "np") {
-            na += amount;
             localStorage.setItem('na', na);
-            document.getElementById("niharika").innerText = `${na}`;
         }
-    
-        if (ka >= na) {
-            ka -= na;
-            na = 0;
-        } else {
-            na -= ka;
-            ka = 0;
-        }
-        document.getElementById("khushi").innerText = ka;
-        document.getElementById("niharika").innerText = na;
-        localStorage.setItem('ka', ka);
-        localStorage.setItem('na', na);
-    }catch(error){
-        alert("please enter valid input")
-    }
-});
+}
+);
 
 kp.addEventListener('click', () => {
     document.getElementById("popup").style.display = "flex";
@@ -54,3 +47,24 @@ np.addEventListener('click', () => {
     document.getElementById("popup").style.display = "flex";
     document.getElementById("popup").dataset.source = "np";
 });
+window.onload = function() {
+    if (localStorage.getItem('ka')) {
+        ka = Number(localStorage.getItem('ka'));
+        if(isNaN(ka)){
+            ;
+        }
+        else{
+            document.getElementById('khushi').innerText = ka;
+        }
+    }
+    if (localStorage.getItem('na')) {
+        na = Number(localStorage.getItem('na'));
+        if(isNaN(na)){
+            ;
+        }
+        else{
+            document.getElementById('niharika').innerText = na; 
+
+        }
+    }
+};
